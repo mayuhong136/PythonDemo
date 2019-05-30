3cbd7afcf70e33f54e6a40332c41830aa167985c
mayuhong136 <51125540+mayuhong136@users.noreply.github.com>
2019-05-30 17:27:50 +0800

    Create DemoA.py

---
+++ DemoA.py
@@ -0,0 +1,7 @@
+import ujson
+
+print ("Hello!Deacy!")
+print ("Hello!Luluvia!")
+print ("Do you have gone to Fanch?")
+print ("No,but my family have gone when I studied at the hight school.")
+print ("Oh!It is a bit prity. " )
