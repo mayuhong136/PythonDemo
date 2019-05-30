b35bb7500a5a74c61fec42084b5467c1992bd6d4
mayuhong136 <51125540+mayuhong136@users.noreply.github.com>
2019-05-30 17:59:17 +0800

    Update DemoA.py

--- DemoA.py
+++ DemoA.py
@@ -3,9 +3,12 @@ import time
 import MQTT
 
 
-print ("Hello!Deacy!")
-print ("Hello!Luluvia!")
-print ("Do you have gone to Fanch?")
-print ("No,but my family have gone when I studied at the hight school.")
-print ("Oh!It is a bit prity. " )
-print ("Yeah!But I think I do like to China rather than Franch. " )
+print ('Hello!Deacy!')
+print ('Hello!Luluvia!')
+print ('Do you have gone to Fanch?')
+print ('No,but my family have gone when I studied at the hight school.')
+print ('Oh!It is a bit prity. " )
+print ('Yeah!But I think I do like to China rather than Franch. ' )
+
+
+
