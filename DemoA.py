d0b8b0df40433cb8f3d3971f5f1470891fbde280
mayuhong136 <51125540+mayuhong136@users.noreply.github.com>
2019-05-30 17:35:03 +0800

    Update DemoA.py

--- DemoA.py
+++ DemoA.py
@@ -1,7 +1,11 @@
 import ujson
+import time
+import MQTT
+
 
 print ("Hello!Deacy!")
 print ("Hello!Luluvia!")
 print ("Do you have gone to Fanch?")
 print ("No,but my family have gone when I studied at the hight school.")
 print ("Oh!It is a bit prity. " )
+print ("Yeah!But I think I do like to China rather than Franch. " )
