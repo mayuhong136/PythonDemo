19271004ccc6365a154758c7a175e5e4ba0c67a8
mayuhong136 <51125540+mayuhong136@users.noreply.github.com>
2019-05-30 16:50:47 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-import os
-
-print ("Hello world!")
+import sys
+import socket
+import machine
+import time
+import network
+sta_if = network.WLAN(network.STA_IF)
+if not sta_if.isconnected():
+  print('connecting to network...')
+  sta_if.active(True)
+  sta_if.connect('myphone', '123456789')
+  #sta_if.connect('nubia', 'yellowlmf1234')
+  while not sta_if.isconnected():
+    pass
+print('network config:', sta_if.ifconfig())
